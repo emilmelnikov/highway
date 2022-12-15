581b35c7a7cf6cb22e6295d7ea7640bc735bab1b
Evgenii Kliuchnikov <eustas@google.com>
2022-12-15 04:30:52 -0800

    WASM: use extmul_half and q15mulr_sat
    PiperOrigin-RevId: 495559071

--- hwy/ops/wasm_128-inl.h
+++ hwy/ops/wasm_128-inl.h
@@ -785,12 +785,8 @@ template <size_t N>
 HWY_API Vec128<uint16_t, N> MulHigh(const Vec128<uint16_t, N> a,
                                     const Vec128<uint16_t, N> b) {
   // TODO(eustas): replace, when implemented in WASM.
-  const auto al = wasm_u32x4_extend_low_u16x8(a.raw);
-  const auto ah = wasm_u32x4_extend_high_u16x8(a.raw);
-  const auto bl = wasm_u32x4_extend_low_u16x8(b.raw);
-  const auto bh = wasm_u32x4_extend_high_u16x8(b.raw);
-  const auto l = wasm_i32x4_mul(al, bl);
-  const auto h = wasm_i32x4_mul(ah, bh);
+  const auto l = wasm_i32x4_extmul_low_u16x8(a.raw, b.raw);
+  const auto h = wasm_i32x4_extmul_high_u16x8(a.raw, b.raw);
   // TODO(eustas): shift-right + narrow?
   return Vec128<uint16_t, N>{
       wasm_i16x8_shuffle(l, h, 1, 3, 5, 7, 9, 11, 13, 15)};
@@ -799,12 +795,8 @@ template <size_t N>
 HWY_API Vec128<int16_t, N> MulHigh(const Vec128<int16_t, N> a,
                                    const Vec128<int16_t, N> b) {
   // TODO(eustas): replace, when implemented in WASM.
-  const auto al = wasm_i32x4_extend_low_i16x8(a.raw);
-  const auto ah = wasm_i32x4_extend_high_i16x8(a.raw);
-  const auto bl = wasm_i32x4_extend_low_i16x8(b.raw);
-  const auto bh = wasm_i32x4_extend_high_i16x8(b.raw);
-  const auto l = wasm_i32x4_mul(al, bl);
-  const auto h = wasm_i32x4_mul(ah, bh);
+  const auto l = wasm_i32x4_extmul_low_i16x8(a.raw, b.raw);
+  const auto h = wasm_i32x4_extmul_high_i16x8(a.raw, b.raw);
   // TODO(eustas): shift-right + narrow?
   return Vec128<int16_t, N>{
       wasm_i16x8_shuffle(l, h, 1, 3, 5, 7, 9, 11, 13, 15)};
@@ -813,18 +805,7 @@ HWY_API Vec128<int16_t, N> MulHigh(const Vec128<int16_t, N> a,
 template <size_t N>
 HWY_API Vec128<int16_t, N> MulFixedPoint15(Vec128<int16_t, N> a,
                                            Vec128<int16_t, N> b) {
-  const DFromV<decltype(a)> d;
-  const RebindToUnsigned<decltype(d)> du;
-
-  const Vec128<uint16_t, N> lo = BitCast(du, Mul(a, b));
-  const Vec128<int16_t, N> hi = MulHigh(a, b);
-  // We want (lo + 0x4000) >> 15, but that can overflow, and if it does we must
-  // carry that into the result. Instead isolate the top two bits because only
-  // they can influence the result.
-  const Vec128<uint16_t, N> lo_top2 = ShiftRight<14>(lo);
-  // Bits 11: add 2, 10: add 1, 01: add 1, 00: add 0.
-  const Vec128<uint16_t, N> rounding = ShiftRight<1>(Add(lo_top2, Set(du, 1)));
-  return Add(Add(hi, hi), BitCast(d, rounding));
+  return Vec128<int16_t, N>{wasm_i16x8_q15mulr_sat(a.raw, b.raw)};
 }
 
 // Multiplies even lanes (0, 2 ..) and returns the double-width result.
